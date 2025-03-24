9248dd88f623be4c2b0c67c50d200c52f8b90287
Jeffry Gonzalez <jeff@hypertheory.com>
2025-03-24 12:36:34 -0400

    before lunch monday

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
-
-import { WelcomeComponent } from './components/welcome.component';
+import { DemoComponent } from './components/demo1/demo1';
 
 @Component({
   selector: 'app-root',
   template: `
     <main class="container mx-auto">
-      <app-welcome />
+      <app-demo-1 /><app-demo-1 /><app-demo-1 /><app-demo-1 /><app-demo-1 /><app-demo-1 /><app-demo-1 /><app-demo-1 /><app-demo-1 /><app-demo-1 />
     </main>
   `,
   styles: [],
-  imports: [WelcomeComponent],
+  imports: [DemoComponent],
 })
 export class AppComponent {}
