c0cb91f3fdd1e1b010d9d503ab34313a3e8e2c15
Jeffry Gonzalez <jeff@hypertheory.com>
2025-03-25 10:16:41 -0400

    prefs setup

---
+++ src/app/pages/profile/components/theme.ts
@@ -0,0 +1,10 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-theme',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [],
+  template: ` <p>Change Your Theme</p> `,
+  styles: ``,
+})
+export class ThemeComponent {}
