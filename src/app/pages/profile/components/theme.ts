2b70ad260f4a1c2dcfd033bea5d17a76a8227825
Jeffry Gonzalez <jeff@hypertheory.com>
2025-03-25 10:33:37 -0400

    can set theme

--- src/app/pages/profile/components/theme.ts
+++ src/app/pages/profile/components/theme.ts
@@ -1,10 +1,51 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  signal,
+  effect,
+} from '@angular/core';
 
 @Component({
   selector: 'app-profile-theme',
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [],
-  template: ` <p>Change Your Theme</p> `,
+  template: `
+    <div class="join">
+      <button
+        [disabled]="selectedTheme() === 'synthwave'"
+        class="join-item"
+        class="btn btn-primary"
+        (click)="selectedTheme.set('synthwave')"
+      >
+        Synthwave
+      </button>
+      <button
+        [disabled]="selectedTheme() === 'cyberpunk'"
+        class="join-item"
+        class="btn btn-primary"
+        (click)="selectedTheme.set('cyberpunk')"
+      >
+        Cyberpunk
+      </button>
+      <button
+        [disabled]="selectedTheme() === 'aqua'"
+        class="join-item"
+        class="btn btn-primary"
+        (click)="selectedTheme.set('aqua')"
+      >
+        Aqua
+      </button>
+    </div>
+  `,
   styles: ``,
 })
-export class ThemeComponent {}
+export class ThemeComponent {
+  selectedTheme = signal<'synthwave' | 'cyberpunk' | 'aqua'>('synthwave');
+
+  constructor() {
+    effect(() => {
+      const theme = this.selectedTheme();
+      document.documentElement.setAttribute('data-theme', theme);
+    });
+  }
+}
